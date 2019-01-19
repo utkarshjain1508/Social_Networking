0dfe18fb5080fce261560c3626f675e7d4620751
cgupta3131 <cgupta3131@gmail.com>
2019-01-20 01:01:21 +0530

    Final AutoGit

--- AutoGit.sh
+++ AutoGit.sh
@@ -5,6 +5,7 @@ myname=$
 git config credential.helper store
 git push origin master	
 
+git config --global credential.helper 'cache --timeout 36000'
 while [[ true ]]; do
 	git add .
 	git commit -m "$USER $(date)"
