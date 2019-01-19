2406a6d00075102e539af58ce1ed85d4010d60d2
cgupta3131 <cgupta3131@gmail.com>
2019-01-20 01:18:18 +0530

    faltu

--- AutoGit.sh
+++ AutoGit.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-myname=$
-
 git config credential.helper store
 git push origin master	
 
 git config --global credential.helper 'cache --timeout 36000'
-while [[ true ]]; do
-	git add .
-	git commit -m "$USER $(date)"
-	git push origin master
 
-	sleep 120
-	git pull origin master
-	sleep 120
-done
+git pull origin master
+
+echo "Enter the Commit Name"
+read mycommit
+#echo $mycommit
+git add .
+git commit -m "$mycommit"
+git push origin master
