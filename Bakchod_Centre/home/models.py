bf24d7488132ee588676af1a0cc997d855a26c44
LavishGulati <lavish00gulati@gmail.com>
2019-01-20 02:45:01 +0530

    Added models

--- Bakchod_Centre/home/models.py
+++ Bakchod_Centre/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100, widget=forms.PasswordInput)
+    contact = models.IntegerField(max_length=20)
+    profilePic = models.ImageField(upload_to='/media/', null=True)  # Check upload_to
+
+    listOfFriends = models.ArrayField(
+        models.CharField(max_length=100)
+    )
