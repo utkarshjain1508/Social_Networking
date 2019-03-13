6a45e2af0b249c7a2b48455f4477e5f91e52c515
LavishGulati <lavish00gulati@gmail.com>
2019-03-13 22:36:57 +0530

    Added friends and posts models

--- Bakchod_Centre/home/models.py
+++ Bakchod_Centre/home/models.py
@@ -16,3 +16,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Connection(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='friend_user')
+    friend = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='friend_friend')
+    connection_time = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Post(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='post_user')
+    post = models.CharField(max_length=1000)
+    post_time = models.DateTimeField(auto_now_add=True, editable=False)
